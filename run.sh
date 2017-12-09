6acc46c8e5969223516ed63bd42491352d9f140c
zocean <zocean636@gmail.com>
2017-12-08 20:41:14 -0500

    update script

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ python src/match_gene.py --refgene data/hg19_refGene.txt --hk_gene data/HK_genes
 bedtools makewindows -g data/hg19F.genome -w 20000 >data/hg19F_20k.bed
 
 python Norma/src/TSA-seq_anno.py --bed data/hg19F_20k.bed --anno cell\ type\ compare/HCT116.bw cell\ type\ compare/HFFc6.bw cell\ type\ compare/K562.bw results/HK_hk.bed.gz results/HK_non_hk.bed.gz --mode signal_mean signal_mean signal_mean anno anno --label HCT116 HFF K562 hk non_hk --genome data/hg19.fa --genome_size data/hg19.genome --output results/20kb_anno.txt
+
+Rscript src/housekeeping_gene.R
