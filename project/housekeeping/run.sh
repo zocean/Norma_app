d1c3771864ed66bb7d6a57c34c0eadc04f1098ac
zocean <zocean636@gmail.com>
2018-01-18 14:58:04 -0500

    update housekeeing pipeline

--- project/housekeeping/run.sh
+++ project/housekeeping/run.sh
@@ -8,4 +8,6 @@ bedtools makewindows -g data/hg19F.genome -w 20000 >data/hg19F_20k.bed
 
 python Norma/src/TSA-seq_anno.py --bed data/hg19F_20k.bed --anno cell\ type\ compare/HCT116.bw cell\ type\ compare/HFFc6.bw cell\ type\ compare/K562.bw results/HK_hk.bed.gz results/HK_non_hk.bed.gz --mode signal_mean signal_mean signal_mean anno anno --label HCT116 HFF K562 hk non_hk --genome data/hg19.fa --genome_size data/hg19.genome --output results/20kb_anno.txt
 
+python Norma/src/TSA-seq_anno.py --update --bed results/20kb_anno.txt --anno data/hg19_Gap.bed.gz --mode count --label Gap --genome data/hg19.fa --genome_size data/hg19.genome --output results/20kb_anno.txt
+
 Rscript src/housekeeping_gene.R
